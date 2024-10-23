116cda5e71265b503bee9d5b620e39ee219517c9
GarbhanK <garbhan@me.com>
2024-10-23 21:51:18 +0100

    lil rename

--- event_generator/main.py
+++ event_generator/main.py
@@ -2,8 +2,8 @@ import json
 
 from fastapi import FastAPI
 
-import endpoints
-import utils
+import event_generator.events as events
+import event_generator.utils as utils
 
 
 app = FastAPI()
@@ -17,7 +17,7 @@ async def root():
 
 @app.get("/payment")
 async def payment():
-    webhook_payload_json = json.dumps(endpoints.get_payment())
+    webhook_payload_json = json.dumps(events.get_payment())
 
     # publish json string to 'payments' channel in Redis
     redis_connection.publish('payments', webhook_payload_json)
@@ -27,7 +27,7 @@ async def payment():
 
 @app.get("/song")
 async def song():
-    webhook_payload_json = json.dumps(endpoints.get_song())
+    webhook_payload_json = json.dumps(events.get_song())
 
     # publish json string to 'songs' channel in Redis
     redis_connection.publish('songs', webhook_payload_json)
